db03192a00d8c61f5fb148437813cfa2dd63d437
seonhyeokjun <tjsrmatn@naver.com>
2022-04-25 23:36:10 +0900

    Initial commit

---
+++ src/test/java/me/slivernine/jwttutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package me.slivernine.jwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
